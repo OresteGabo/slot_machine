ecaa207a5ee9ab37a040222dc9c8eb69e25f3cb9
oreste <muhirwa.g.oreste@gmail.com>
2022-10-22 17:29:01 +0200

    La procédure pour saisir le nombre de lignes pour le pari, la fonction main, ainsi que le NOMBRE_LIGNES_MAX pour nous aider à limiter

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
-
+NOMBRE_LIGNES_MAX = 3
 # La fonction pour faire un dépot
 # Le montant doit etre positif
-def deposit():
+def depot():
     """
 
     :rtype: Return int
@@ -19,7 +19,31 @@ def deposit():
             print("Le montant doit etre numerique")
     return amount
 
+# L'utilisateur doit saisir lenombre de ligne
+# Le nombre doit etre d'abord numerique, et superieur à zero
+def get_nombre_lignes():
+    """
+
+    :rtype: Return int
+    """
+    while True:
+        lignes =input("Combien de ligne souhaitez-vous parier (1-"+ str(NOMBRE_LIGNES_MAX)+ ") $")
+        if lignes.isdigit():
+            lignes=int(lignes)
+            if lignes >= 0:
+                break
+            else:
+                print("Le montant doit etre superieur à 0")
+        else:
+            print("Le montant doit etre numerique")
+    return lignes
 
 
-if __name__ == '__main__':
-    deposit()
+
+def main():
+    balance=depot()
+    lignes=get_nombre_lignes()
+    print(balance,lignes)
+
+
+main()
